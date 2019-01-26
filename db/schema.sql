b44f38185b997f1c819b73ac60c457ae1cbc1e4c
Andy <andy.harringa@gmail.com>
2019-01-25 19:50:48 -0700

    Still not functioning

--- db/schema.sql
+++ db/schema.sql
@@ -3,8 +3,8 @@ CREATE DATABASE burgers_db;
 USE burgers_db;
 
 CREATE TABLE burgers (
-  id INT NOT NULL AUTO_INCREMENT, /*primary key*/
+  id INT NOT NULL AUTO_INCREMENT,
   burger_name VARCHAR(100) NOT NULL,
   devoured BOOLEAN NOT NULL,
-  PRIMARY KEY (id) /*primary key*/
+  PRIMARY KEY (id)
 );
